d4c310667f986d91c00f0177f412609484823a79
Addiel <addieljuarez@gmail.com>
2013-11-16 10:23:27 -0600

    login

--- Resources/ui/common/login.js
+++ Resources/ui/common/login.js
@@ -1,9 +1,19 @@
 function Login(){
 	var self = Titanium.UI.createWindow({
-		backgroundColor:'red',
+		backgroundColor:'#fff',
+	});
+	
+	var inputName = Titanium.UI.createTextField({
+		top:200,
+		height:40,
+		width:250,
+		borderColor:'#000',
+		borderRadius:5,
+		hintText:'inserta tu nombre'
 	});
 	
 	
+	self.add(inputName);
 	return self;
 }
 module.exports = Login;
